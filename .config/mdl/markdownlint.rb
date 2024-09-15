3380b5d8fcb0ea25634f4983f2e0a3887b8a0c00
Guillermo Garcia <ahioros@users.noreply.github.com>
2024-09-15 10:57:35 -0500

    add config markdown lint

---
+++ .config/mdl/markdownlint.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+
+all
+
+rule 'MD013', :line_length => 1000
+
+rule 'MD026', :punctuation => ".,;"
+
+#Resource: https://github.com/DavidAnson/markdownlint/blob/main/schema/.markdownlint.yaml
